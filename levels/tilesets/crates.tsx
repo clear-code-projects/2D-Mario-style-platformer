2c7acc3095b54fc2272c01738b94a45a55dfade4
clear-code-projects <62236986+clear-code-projects@users.noreply.github.com>
2021-08-23 20:05:30 +0100

    Delete levels directory

--- levels/tilesets/crates.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="crates" tilewidth="58" tileheight="42" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="58" height="42" source="../../graphics/terrain/crate.png"/>
- </tile>
-</tileset>
